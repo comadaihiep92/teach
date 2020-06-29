a7026aa4940c129e85f77987d467637c67139fa3
comadaihiep92 <motden100@gmail.com>
2020-06-29 11:57:00 +0700

    add 2 screen

---
+++ src/components/AppointmentCenter/AppointmentCenter.tsx
@@ -0,0 +1,225 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  IonPage,
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonImg,
+  IonBackButton,
+  IonButtons,
+  IonList,
+  IonItem,
+  IonText,
+} from "@ionic/react";
+
+import "./AppointmentCenter.scss";
+
+import iconBack from "../../images/icon-back.png";
+import iconClock from "../../images/icon-clock.png";
+import img1 from "../../images/img-parent.png";
+import img2 from "../../images/img-parent2.png";
+
+const AppointmentCenter: React.FC = () => {
+  return (
+    <IonPage className="appointment">
+      <IonHeader className="ion-no-border appointment__header">
+        <IonToolbar className="appointment__toolbar">
+          <IonButtons slot="start">
+            <Link to="/">
+              <IonImg className="icon icon-back" src={iconBack} alt="" />
+            </Link>
+          </IonButtons>
+
+          <IonTitle className="appointment__title-header">
+            Appointment Center
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonList className="appointment__list">
+          <IonText className="appointment__title">
+            Parents are sure to be present
+          </IonText>
+          <IonList className="appointment__item ion-no-padding">
+            <IonList className="appointment__sub-item">
+              <IonText className="appointment__reminder">
+                Appointment reminder
+              </IonText>
+              <IonList className="appointment__times">
+                <IonImg className="icon icon--clock" src={iconClock} alt="" />
+                <IonText className="appointment__date">Friday: at 4 pm</IonText>
+              </IonList>
+              <IonList className="appointment__list-image">
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+              </IonList>
+            </IonList>
+            <IonList className="appointment__sub-item">
+              <IonText className="appointment__reminder">
+                Appointment reminder
+              </IonText>
+              <IonList className="appointment__times">
+                <IonImg className="icon icon--clock" src={iconClock} alt="" />
+                <IonText className="appointment__date">Friday: at 4 pm</IonText>
+              </IonList>
+              <IonList className="appointment__list-image">
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+              </IonList>
+            </IonList>
+            <IonList className="appointment__sub-item">
+              <IonText className="appointment__reminder">
+                Appointment reminder
+              </IonText>
+              <IonList className="appointment__times">
+                <IonImg className="icon icon--clock" src={iconClock} alt="" />
+                <IonText className="appointment__date">Friday: at 4 pm</IonText>
+              </IonList>
+              <IonList className="appointment__list-image">
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+              </IonList>
+            </IonList>
+            <IonList className="appointment__sub-item">
+              <IonText className="appointment__reminder">
+                Appointment reminder
+              </IonText>
+              <IonList className="appointment__times">
+                <IonImg className="icon icon--clock" src={iconClock} alt="" />
+                <IonText className="appointment__date">Friday: at 4 pm</IonText>
+              </IonList>
+              <IonList className="appointment__list-image">
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img1} alt="" />
+                  <IonText className="appointment__name">Jack</IonText>
+                </IonList>
+                <IonList className="appointment__item-image">
+                  <IonImg className="img img--parent" src={img2} alt="" />
+                  <IonText className="appointment__name">Ali</IonText>
+                </IonList>
+              </IonList>
+            </IonList>
+          </IonList>
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default AppointmentCenter;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,112 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonImg,
+  IonText,
+  IonList,
+  IonItem,
+  IonToolbar,
+  IonAlert,
+  IonLabel,
+  IonRouterLink,
+} from "@ionic/react";
+import React from "react";
+import "./Home.scss";
+
+import iconPicture from "../images/icon-mypicture.png";
+import iconMessage from "../images/icon-message.png";
+import iconSchool from "../images/icon-document.png";
+import iconInfoChild from "../images/icon-infochild.png";
+import iconAppointment from "../images/icon-appointment.png";
+import iconAttendance from "../images/icon-attendance.png";
+import iconNotification from "../images/icon-notification.png";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <div className="home">
+          <IonHeader className="ion-no-border home__header">
+            <IonTitle className="ion-text-center home__title-header">
+              Home
+            </IonTitle>
+            <IonList className="home__noti">
+              <IonImg
+                className="icon icon--noti"
+                src={iconNotification}
+                alt=""
+              />
+              <IonLabel className="home__label">1</IonLabel>
+            </IonList>
+          </IonHeader>
+
+          <IonList className="home__list">
+            <IonRouterLink href="#" className="home__link">
+              <IonList className="home__item">
+                <IonImg
+                  className="home__image"
+                  src={iconPicture}
+                  alt=""
+                ></IonImg>
+                <IonText className="home__title">My Pictures</IonText>
+              </IonList>
+            </IonRouterLink>
+            <IonRouterLink href="#" className="home__link">
+              <IonList className="home__item">
+                <IonImg
+                  className="home__image"
+                  src={iconMessage}
+                  alt=""
+                ></IonImg>
+                <IonText className="home__title">Message Center</IonText>
+              </IonList>
+            </IonRouterLink>
+            <IonRouterLink href="#" className="home__link">
+              <IonList className="home__item">
+                <IonImg
+                  className="home__image"
+                  src={iconSchool}
+                  alt=""
+                ></IonImg>
+                <IonText className="home__title">School Documents</IonText>
+              </IonList>
+            </IonRouterLink>
+            <IonRouterLink href="#" className="home__link">
+              <IonList className="home__item">
+                <IonImg
+                  className="home__image"
+                  src={iconInfoChild}
+                  alt=""
+                ></IonImg>
+                <IonText className="home__title">
+                  Child Daily information
+                </IonText>
+              </IonList>
+            </IonRouterLink>
+            <IonRouterLink href="/appointmentcenter" className="home__link">
+              <IonList className="home__item">
+                <IonImg
+                  className="home__image"
+                  src={iconAppointment}
+                  alt=""
+                ></IonImg>
+                <IonText className="home__title">Appointment Center</IonText>
+              </IonList>
+            </IonRouterLink>
+            <IonRouterLink href="#" className="home__link">
+              <IonList className="home__item">
+                <IonImg
+                  className="home__image"
+                  src={iconAttendance}
+                  alt=""
+                ></IonImg>
+                <IonText className="home__title">Attendance</IonText>
+              </IonList>
+            </IonRouterLink>
+          </IonList>
+        </div>
       </IonContent>
     </IonPage>
   );
